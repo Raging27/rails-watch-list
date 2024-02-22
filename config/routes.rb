e80a158dc080951db8858813c6494c00514f4043
omar haizoun <omar.haizoun.m@gmail.com>
2024-02-22 16:26:37 +0100

    ....

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-
-  root to: "movies#index"
+  root to:"movies#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
