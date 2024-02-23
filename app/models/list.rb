37b556e1725b3f0c84e0ab8d2cc9744d2e2bf80a
omar haizoun <omar.haizoun.m@gmail.com>
2024-02-23 14:26:51 +0100

    .......

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,8 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
+  has_rich_text :rich_body
+
 
   validates :name, uniqueness: true, presence: true
 end
---
+++ db/migrate/20240223123014_migrate_bodyto_rich_body_lists.rb
@@ -0,0 +1,4 @@
+class MigrateBodytoRichBodyLists < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
