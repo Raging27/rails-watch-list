697d010a7431f903dfb691b95a23463b13e201c2
omar haizoun <omar.haizoun.m@gmail.com>
2024-02-23 10:56:38 +0100

    copied part of

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,9 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
-    @movies = Movie.all
-  end
-
-  def index
-    @bookmarks = Bookmark.all
   end
 
   def create
@@ -16,23 +12,27 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new
+      @review = Review.new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
-  def set_list
-    @list = List.find(params[:list_id])
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  def bookmark_params
-    params.require(:bookmark).permit(:movie_id, :comment)
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
